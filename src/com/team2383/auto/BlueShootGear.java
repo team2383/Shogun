cf724c51ccee254e9b3beb458a8e79372f094d65
Anurag Akkiraju <anurag.akkiraju@gmail.com>
2017-10-18 19:48:27 -0400

    wed changes

--- src/com/team2383/auto/BlueShootGear.java
+++ src/com/team2383/auto/BlueShootGear.java
@@ -18,15 +18,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class BlueShootGear extends CommandGroup {
     public BlueShootGear() {
     	addSequential(new DisableFlap());
-    	addSequential(new DriveDistance(68, 0.9,Gear.LOW, true));
-    	addSequential(new GyroTurn(1.0,57,1.9));
+    	addSequential(new DriveDistance(72, 0.9,Gear.LOW, true));
+    	addSequential(new GyroTurn(0.8,57,1.9));
     	//addSequential(new DriveDistance(73, 0.7, Gear.LOW, true));
     	//addSequential(new DriveDistance(60.0, 0.6, Gear.LOW, true));
     	addSequential(new AutoDriveStraight(() -> -0.8,1.0));
     	//addSequential(new AutoDriveStraight(() -> 0.5 , 0.25));
     	addParallel(new EnableDoor());
     	addSequential(new AutoDriveStraight(() -> 0.01,1.5));
-    	addParallel(new DriveDistance(-40.0, 0.9, Gear.LOW, true));
+    	addParallel(new DriveDistance(-32.5, 0.9, Gear.LOW, true));
     	addSequential(new AutoShoot(() -> 3600,10.0));
     }
 }
